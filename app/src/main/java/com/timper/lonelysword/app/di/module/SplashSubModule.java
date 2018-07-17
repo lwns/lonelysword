981b83d26160527755600dc6072c3bed3990bd76
ytplylm <871373988@qq.com>
2018-07-17 11:09:11 +0800

    feat:

--- app/src/main/java/com/timper/lonelysword/app/di/module/SplashSubModule.java
+++
@@ -1,8 +0,0 @@
-//// Generated code from lonely sword. Do not modify!
-//package com.timper.lonelysword.app.di.module;
-//
-//import dagger.Module;
-//
-//@Module
-//public abstract class SplashSubModule {
-//}
